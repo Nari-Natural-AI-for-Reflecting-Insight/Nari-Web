cdf847dd86121f04c959fea3f34a4267208567e0
daeun <gbajs754@naver.com>
2025-04-27 15:07:43 +0900

    feat : zod로 회원가입 검증 강화

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,39 +1,39 @@
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import useSignupMutation from '@/features/auth/hooks/useSignupMutation';
-
-type Inputs = {
-  email: string;
-  password: string;
-  nickname: string;
-};
+import { SignupInputs, signupSchema } from '@/features/auth/schema';
 
 const Signup = () => {
   const signupMutation = useSignupMutation();
-  const { register, handleSubmit } = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SignupInputs>({
+    resolver: zodResolver(signupSchema),
+  });
 
-  const onSubmit: SubmitHandler<Inputs> = (data) => {
+  const onSubmit: SubmitHandler<SignupInputs> = (data) => {
     signupMutation.mutate(data);
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder="이메일을 입력해주세요" {...register('email')} />
+      {errors.email && <p>{errors.email.message}</p>}
       <input
-        type="email"
-        placeholder="이메일을 입력해주세요"
-        {...register('email')}
-      />
-      <input
-        type="text"
         autoComplete="current-username"
         placeholder="닉네임을 입력해주세요"
         {...register('nickname')}
       />
+      {errors.nickname && <p>{errors.nickname.message}</p>}
       <input
         type="password"
         autoComplete="current-password"
         placeholder="비밀번호를 입력해주세요"
         {...register('password')}
       />
+      {errors.password && <p>{errors.password.message}</p>}
       <input type="submit" />
     </form>
   );
