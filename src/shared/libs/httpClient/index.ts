0d058ae4e58b627e70060a9578c7f412f965580c
daeun <gbajs754@naver.com>
2025-05-01 20:54:30 +0900

    feat : axios error 타입 추가

--- src/shared/libs/httpClient/index.ts
+++ src/shared/libs/httpClient/index.ts
@@ -1,7 +1,14 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const httpClient = axiosInstance;
+
+httpClient.interceptors.response.use(
+  (response) => response.data,
+  (error: AxiosError) => {
+    return Promise.reject(error.response);
+  },
+);
