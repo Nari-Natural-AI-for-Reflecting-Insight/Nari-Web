497cf8d1b3192eeea04cfe271ef6a0dd4c39d80b
daeun <gbajs754@naver.com>
2025-05-01 20:53:23 +0900

    feat : toast 오류 메세지 추가

--- src/features/auth/hooks/useSignupMutation.ts
+++ src/features/auth/hooks/useSignupMutation.ts
@@ -1,9 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
+import { AxiosResponse } from 'axios';
+import { toast } from 'sonner';
 import authApi from '@/features/auth/apis';
+import { ErrorData } from '@/shared/types/error';
 
 const useSignupMutation = () => {
   return useMutation({
     mutationFn: authApi.postSignup,
+    onError: ({ data }: AxiosResponse<ErrorData>) => {
+      toast.error(data.error.message);
+    },
   });
 };
 
