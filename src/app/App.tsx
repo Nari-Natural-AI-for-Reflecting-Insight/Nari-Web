69b4de01ce2fcae05bbdf98ed183b269670f24ee
daeun <gbajs754@naver.com>
2025-05-01 20:53:23 +0900

    feat : toast 오류 메세지 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,12 @@
 import { QueryClientProvider } from '@/app/providers/QueryClientProvider';
 import { RouterProvider } from '@/app/providers/RouterProvider';
+import ToastProvider from '@/app/providers/ToastProvider';
 
 function App() {
   return (
     <QueryClientProvider>
       <RouterProvider />
+      <ToastProvider />
     </QueryClientProvider>
   );
 }
