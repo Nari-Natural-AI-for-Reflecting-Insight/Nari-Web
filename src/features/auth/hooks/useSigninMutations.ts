14ccece8db7b47ec05e30f7c5c83c9f9327e6d10
Curry4182 <kangbk4182@naver.com>
2025-05-06 20:49:14 +0900

    feat : 로그인 성공 후 index 페이지로 이동하도록 기능 추가

--- src/features/auth/hooks/useSigninMutations.ts
+++ src/features/auth/hooks/useSigninMutations.ts
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router';
 import { useMutation } from '@tanstack/react-query';
 import { AxiosResponse } from 'axios';
 import { toast } from 'sonner';
@@ -8,13 +9,17 @@ import { ErrorData } from '@/shared/types/error';
 
 
 const useSigninMutation = () => {
+
+  const navigate = useNavigate();
+
   return useMutation({
     mutationFn: authApi.postSignin,
     onSuccess: (response:PostSigninResponse) => {
         const { accessToken } = response.data;
         accessTokenStorage.set(accessToken);     
-         
+        
         toast.success('로그인에 성공했습니다.');
+        navigate('/');
     },
     onError: ({ data }: AxiosResponse<ErrorData>) => {
       toast.error(data.error.message);
