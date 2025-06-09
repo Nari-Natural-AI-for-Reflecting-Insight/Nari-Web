571fc3491af7f5f72a721eb1fbe34b599101f5ac
daeun <gbajs754@naver.com>
2025-06-09 17:55:47 +0900

    feat : home 레이아웃 구현

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router';
 import PrivateRoute from '@/app/router/PrivateRoute';
 import { RouterErrorBoundary } from '@/app/providers/RouterErrorBoundary';
+import AppLayout from '@/shared/layout/AppLayout';
 import Home from '@/pages/Home';
 import My from '@/pages/My';
 import Signin from '@/pages/Signin';
@@ -9,6 +10,7 @@ import Signup from '@/pages/Signup';
 export const router = createBrowserRouter([
   {
     path: '/',
+    element: <AppLayout />,
     ErrorBoundary: RouterErrorBoundary,
     children: [
       {
