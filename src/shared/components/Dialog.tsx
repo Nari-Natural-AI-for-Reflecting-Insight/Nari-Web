d4c600da14ac96a28d31b3f8585127834ed9e779
daeun <gbajs754@naver.com>
2025-08-24 16:34:45 +0900

    feat : Dialog 컴포넌트 수정

--- src/shared/components/Dialog.tsx
+++ src/shared/components/Dialog.tsx
@@ -1,13 +1,15 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, ReactNode, SetStateAction } from 'react';
 import * as RadixDialog from '@radix-ui/react-dialog';
-import Button from '@/shared/components/Button';
 
 interface DialogProps {
   title?: string;
   description?: string;
   open: boolean;
   onOpenChange: Dispatch<SetStateAction<boolean>>;
-  onLeftButtonClick?: () => void;
+  renderRightButton?: () => ReactNode;
+  renderLeftButton?: () => ReactNode;
+  renderBottomButton?: () => ReactNode;
+  isPriority?: boolean;
 }
 
 const Dialog = ({
@@ -15,23 +17,30 @@ const Dialog = ({
   description,
   open,
   onOpenChange,
-  onLeftButtonClick,
+  renderRightButton,
+  renderLeftButton,
+  renderBottomButton,
+  isPriority,
 }: DialogProps) => {
   return (
     <RadixDialog.Root open={open} onOpenChange={onOpenChange}>
       <RadixDialog.Portal>
         <RadixDialog.Overlay className="fixed inset-0 bg-black/40" />
-        <RadixDialog.Content className="fixed left-1/2 top-1/2 max-h-[85vh] w-[90vw] max-w-[500px] -translate-x-1/2 -translate-y-1/2 rounded-md bg-white p-[25px]">
-          <RadixDialog.Title>{title}</RadixDialog.Title>
-          <RadixDialog.Description>{description}</RadixDialog.Description>
-          <Button onClick={onLeftButtonClick}>확인</Button>
-          <Button
-            onClick={() => {
-              onOpenChange(false);
-            }}
-          >
-            취소
-          </Button>
+        <RadixDialog.Content className="fixed left-1/2 top-1/2 max-h-[276vh] w-full max-w-[345px] -translate-x-1/2 -translate-y-1/2 rounded-4xl p-[25px] bg-[rgba(64,65,70,0.5)] backdrop-blur-[10px] shadow-[0_20px_50px_rgba(0,0,0,0.25)] flex flex-col items-center gap-4">
+          <RadixDialog.Title className="text-xl font-kbo text-white flex gap-2.5">
+            {isPriority && (
+              <img src="public/icons/priority.svg" className="w-8 h-auto" />
+            )}
+            {title}
+          </RadixDialog.Title>
+          <RadixDialog.Description className="text-white text-sm whitespace-pre-line text-center">
+            {description}
+          </RadixDialog.Description>
+          <div className="flex justify-evenly gap-4 w-full">
+            {renderLeftButton && renderLeftButton()}
+            {renderRightButton && renderRightButton()}
+          </div>
+          <div>{renderBottomButton && renderBottomButton()}</div>
         </RadixDialog.Content>
         <RadixDialog.Close />
       </RadixDialog.Portal>
